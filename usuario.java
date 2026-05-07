d5cc2ab9049869c57fff67cda8ae7920d352eb57
Alejandro Gonzalez <algoma.agm@gmail.com>
2026-05-07 17:10:40 +0200

    main.java

---
+++ usuario.java
@@ -0,0 +1,18 @@
+public class Usuario {
+
+    private String email;
+    private String password;
+
+    public Usuario(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
